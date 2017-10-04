a65f051ee163b04dbfe96202c087fa29cea7d8b1
fatcamera <caobinbin@live.com>
2017-10-04 10:24:52 +0800

    update translation

--- languages/zh_CN.ts
+++ languages/zh_CN.ts
@@ -4,32 +4,32 @@
 <context>
     <name>DataTableModel</name>
     <message>
-        <location filename="../data_panel.py" line="214"/>
+        <location filename="../data_panel.py" line="215"/>
         <source>Date</source>
         <translation>日期</translation>
     </message>
     <message>
-        <location filename="../data_panel.py" line="216"/>
+        <location filename="../data_panel.py" line="217"/>
         <source>Room</source>
         <translation>房型</translation>
     </message>
     <message>
-        <location filename="../data_panel.py" line="218"/>
+        <location filename="../data_panel.py" line="219"/>
         <source>Source</source>
         <translation>渠道</translation>
     </message>
     <message>
-        <location filename="../data_panel.py" line="220"/>
+        <location filename="../data_panel.py" line="221"/>
         <source>Price</source>
         <translation>价格</translation>
     </message>
     <message>
-        <location filename="../data_panel.py" line="222"/>
+        <location filename="../data_panel.py" line="223"/>
         <source>Commission</source>
         <translation>佣金</translation>
     </message>
     <message>
-        <location filename="../data_panel.py" line="224"/>
+        <location filename="../data_panel.py" line="225"/>
         <source>Comment</source>
         <translation>备注</translation>
     </message>
@@ -37,155 +37,160 @@
 <context>
     <name>MainStatusBar</name>
     <message>
-        <location filename="../main_statusbar.py" line="37"/>
-        <source>Room Nights: {}, Income: {:.2f}, Expense: {:.2f}, Profit: {:.2f}, Share: {:.2f}</source>
-        <translation>间夜: {}, 收入: {:.2f}, 支出: {:.2f}, 利润: {:.2f}, 分成: {:.2f}</translation>
+        <location filename="../main_statusbar.py" line="40"/>
+        <source>Room Nights: {}, Income: {:.2f}, Expense: {:.2f}, Profit: {:.2f}</source>
+        <translation>间夜: {}, 收入: {:.2f}, 支出: {:.2f}, 利润: {:.2f}</translation>
+    </message>
+    <message>
+        <location filename="../main_statusbar.py" line="45"/>
+        <source>Yanyan&apos;s Income: {:.2f}, Neighbour&apos;s Share: {:.2f}</source>
+        <translation>艳艳收入: {:.2f}, 邻居分成: {:.2f}</translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../main_window.py" line="97"/>
+        <location filename="../main_window.py" line="92"/>
         <source>&amp;Save</source>
         <translation>保存(&amp;S)</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="108"/>
+        <location filename="../main_window.py" line="103"/>
         <source>E&amp;xit</source>
         <translation>退出(&amp;X)</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="112"/>
+        <location filename="../main_window.py" line="107"/>
         <source>Exit</source>
         <translation>退出</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="115"/>
+        <location filename="../main_window.py" line="110"/>
         <source>&amp;Undo</source>
         <translation>撤销(&amp;U)</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="120"/>
+        <location filename="../main_window.py" line="115"/>
         <source>&amp;Redo</source>
         <translation>重做(&amp;R)</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="125"/>
+        <location filename="../main_window.py" line="120"/>
         <source>&amp;Documentation</source>
         <translation>手册(&amp;D)</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="129"/>
+        <location filename="../main_window.py" line="124"/>
         <source>Open Documentation</source>
         <translation>打开手册</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="141"/>
+        <location filename="../main_window.py" line="136"/>
         <source>&amp;File</source>
         <translation>文件(&amp;F)</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="148"/>
+        <location filename="../main_window.py" line="143"/>
         <source>&amp;Edit</source>
         <translation>编辑(&amp;M)</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="154"/>
+        <location filename="../main_window.py" line="149"/>
         <source>&amp;Help</source>
         <translation>帮助(&amp;H)</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="260"/>
+        <location filename="../main_window.py" line="255"/>
         <source>Save Successed.</source>
         <translation>保存成功。</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="265"/>
+        <location filename="../main_window.py" line="260"/>
         <source>Save Failed.</source>
         <translation>保存失败。</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="159"/>
+        <location filename="../main_window.py" line="154"/>
         <source>File Toolbar</source>
         <translation>文件工具栏</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="165"/>
+        <location filename="../main_window.py" line="160"/>
         <source>Edit Toolbar</source>
         <translation>编辑工具栏</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="134"/>
+        <location filename="../main_window.py" line="129"/>
         <source>&amp;About {}</source>
         <translation>关于 {}(&amp;A)</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="287"/>
+        <location filename="../main_window.py" line="282"/>
         <source>About {}</source>
         <translation>关于 {}</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="74"/>
+        <location filename="../main_window.py" line="69"/>
         <source>&amp;Open</source>
         <translation>打开(&amp;O)</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="213"/>
+        <location filename="../main_window.py" line="208"/>
         <source>Open Bill</source>
         <translation>打开账单</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="82"/>
+        <location filename="../main_window.py" line="77"/>
         <source>&amp;New</source>
         <translation>新建(&amp;N)</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="86"/>
+        <location filename="../main_window.py" line="81"/>
         <source>New Bill</source>
         <translation>新建账单</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="90"/>
+        <location filename="../main_window.py" line="85"/>
         <source>&amp;Import</source>
         <translation>导入(&amp;I)</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="231"/>
+        <location filename="../main_window.py" line="226"/>
         <source>Import Web Bill</source>
         <translation>导入网络账单</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="251"/>
+        <location filename="../main_window.py" line="246"/>
         <source>Save Bill</source>
         <translation>保存账单</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="193"/>
+        <location filename="../main_window.py" line="188"/>
         <source>Save changes before closing?</source>
         <translation>关闭前保存文件？</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="251"/>
+        <location filename="../main_window.py" line="246"/>
         <source>Bill (*.csv)</source>
         <translation>账单 (*.csv)</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="231"/>
+        <location filename="../main_window.py" line="226"/>
         <source>Web Bill (*.xls *.xlsx)</source>
         <translation>网络账单 (*.xls *.xlsx)</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="287"/>
+        <location filename="../main_window.py" line="282"/>
         <source>This application can be used to do accounting.</source>
         <translation>这个应用可以协助小房子对账。</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="237"/>
+        <location filename="../main_window.py" line="232"/>
         <source>Import Successed.</source>
         <translation>导入成功。</translation>
     </message>
     <message>
-        <location filename="../main_window.py" line="241"/>
+        <location filename="../main_window.py" line="236"/>
         <source>Import Failed.</source>
         <translation>导入失败。</translation>
     </message>
